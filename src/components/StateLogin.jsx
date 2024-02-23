b0f40ec216a7b29799dcc421b054e1ef711176df
Mehmet <mathdenizi@gmail.com>
2024-02-23 21:42:19 +0100

    Getting User Input via Refs

---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+  function handleSubmit() {
+    event.preventDefault();
+    console.log("Entered values: " + enteredValues.email);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+
+  /* function handleEmailChange() {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange() {
+    setEnteredPassword(event.target.value);
+  }*/
+  return (
+    <form>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button" onClick={handleSubmit}>
+          Login
+        </button>
+      </p>
+    </form>
+  );
+}
