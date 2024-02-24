ed64c156f666cb8c5820fafdeb83441678bd3696
Mehmet <mathdenizi@gmail.com>
2024-02-24 01:35:36 +0100

    validating input after submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export default function Login() {
+  const [inputIsInvalid, setInputIsInvalid] = useState({
+    email: false,
+    password: false,
+  });
+
   const email = useRef();
   const password = useRef();
 
@@ -7,9 +12,31 @@ export default function Login() {
     event.preventDefault();
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+
+    const emailIsInvalid = !enteredEmail.includes("@");
+    const passwordIsInvalid = enteredPassword.length < 5;
+    if (emailIsInvalid || passwordIsInvalid) {
+      if (emailIsInvalid) {
+        setInputIsInvalid((prev) => ({
+          ...prev,
+          email: true,
+        }));
+      }
+      if (passwordIsInvalid) {
+        setInputIsInvalid((prev) => ({ ...prev, password: true }));
+      }
+      return;
+    }
+
+    setInputIsInvalid({
+      email: false,
+      password: false,
+    });
+
     console.log("Entered values: " + enteredEmail + " - " + enteredPassword);
-    //email.current.value=''  //reset is not good in ref, we change the dom here so not recommended
-    event.target.reset();
+    email.current.value = ""; //reset is not good in ref, we change the dom here so not recommended
+    password.current.value = "";
+    //event.target.reset(); //not work
   }
 
   return (
@@ -20,11 +47,19 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {inputIsInvalid.email && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input id="password" type="password" name="password" ref={password} />
+          <div className="control-error">
+            {inputIsInvalid.password && (
+              <p>Please enter at least 5 characters</p>
+            )}
+          </div>
         </div>
       </div>
 
